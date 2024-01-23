03556a1a425b437368db442cfda44b1eee1e1087
thao nguyen <thao.nguyen@allcommerce.vn>
2024-01-23 18:09:44 +0700

    seven commit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,11 +6,11 @@ function Contact() {
         <div className="contact-wrapper">
           <h2>Contact us</h2>
           <p className="desc">Any question? Reach out to us and we’ll get back to you shortly.</p>
-          <p>admin@superlott.io</p>
+          <p className="desc-important">admin@superlott.io</p>
           <form action="">
-            <input type="text" placeholder="Your Name" className="contact-input" />
-            <input type="text" placeholder="Your Email" className="contact-input" />
-            <input type="text" placeholder="Your Message" className="contact-input" />
+            <input type="text" placeholder="Your Name" className="form-input" />
+            <input type="text" placeholder="Your Email" className="form-input" />
+            <input type="text" placeholder="Your Message" className="form-input" />
           </form>
         </div>
       </div>
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+function Footer() {
+  return (
+    <>
+    <div className="footer">
+      <p>Copyright © 2023 by JP Finance Lab, Cyprus | admin@superlott.io</p>
+    </div>
+    </>
+  );
+}
+export default Footer;
